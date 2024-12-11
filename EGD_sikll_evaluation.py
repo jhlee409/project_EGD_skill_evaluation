ea2dff62dd3fd1a20795c625ba26678076b4b5d4
jhlee409 <jhlee409@gmail.com>
2024-12-11 16:33:02 +0900

    4

--- EGD_sikll_evaluation.py
+++ EGD_sikll_evaluation.py
@@ -248,7 +248,7 @@ for (path, dir, files) in os.walk(dirname):
         print(':test 폴더에 있는 test.png를 expert photo.pptx와 비교해 보고 만족스러우면 파일을 제출해 주세요')
         str3 = 'pass.'
     else:
-        print('\nEGD 수행이 적절하게 진행되지 못했습니다. 1단계 불���격입니다. 다시 시도해 주세요 \n')
+        print('\nEGD 수행이 적절하게 진행되지 못했습니다. 1단계 불합격입니다. 다시 시도해 주세요 \n')
         str3 = 'failure.'
 
 w = 12
