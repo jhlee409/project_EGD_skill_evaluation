2b9ef829fa64d1ff355f1cc6a059e857206c59d0
jhlee409 <jhlee409@gmail.com>
2024-12-12 20:20:52 +0900

    1

--- EGD_sikll_evaluation.py
+++ EGD_sikll_evaluation.py
@@ -14,12 +14,24 @@ from math import atan2, degrees
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 
-# Firebase 초기화
+ # Check if Firebase app has already been initialized
 if not firebase_admin._apps:
-    cred = credentials.Certificate(st.secrets["firebase_credentials"])
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': st.secrets["firebase_storage_bucket"]
+    # Streamlit Secrets에서 Firebase 설정 정보 로드
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace('\\n', '\n'),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"]
     })
+    firebase_admin.initialize_app(cred)
+
 
 bucket = storage.bucket()
 
