70914ffca63d8ede4bc3308880390cfa88304d4a
jhlee409 <jhlee409@gmail.com>
2024-12-15 10:23:09 +0900

    51

--- EGD_skill_evaluation.py
+++ EGD_skill_evaluation.py
@@ -245,6 +245,11 @@ if uploaded_files:
             clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
             clf.fit(x_train_scaled)
 
+            st.write("---")
+            st.subheader("-기존 훈련 데이터 로드-")
+            st.write(f"기존 훈련 데이터 수: {len(x_train)}")
+            st.write(f"기존 훈련 데이터: {x_train}")
+
             st.subheader("-최종 판정-")
 
             y_pred_test = clf.predict(x_test_scaled)
